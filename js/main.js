85b87838522ac56eca5094d122869a72a702c9ee
Emilija92 <pkovacevicemilija@gmail.com>
2021-09-17 20:03:23 +0200

    nav

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var typed = new Typed('.element', {
+    strings: ['Arabic', 'Chinese', 'English', 'Dutch', 'Polski','French'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    smartBackspace: true,
+    loop: true,
+    loopCount: Infinity,
+    showCursor: false
+});
